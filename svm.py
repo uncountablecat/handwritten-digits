5f0b47a8144b895593b9fee0a64a4b90c36b1ade
Chao <chaoguo@wifi-131-179-40-186.host.ucla.edu>
2016-01-05 13:28:32 -0800

    SVM with polynomial kernel

--- svm.py
+++ svm.py
@@ -1,18 +1,37 @@
 from numpy import genfromtxt
-from sklearn import svm
+from sklearn.svm import SVC
 import pandas as pd
 
 # read csv file
 raw_set = pd.read_csv('training_set.csv', sep=',', header=0)
 
-# testing purpose
-print 'Original dataset:'
+# testing purpose: dimension check
+print 'Original dataset:',
 print(raw_set.shape)
 
-training_set = raw_set.iloc[0:40000,:] # pandas uses 0 index, unlike R
-test_set = raw_set.iloc[40000:,:]
-print(training_set.shape)
-print(test_set.shape)
+training_set = raw_set.iloc[0:4000,:] # pandas uses 0 index, unlike R
+test_set = raw_set.iloc[4000:5000,:]
 
-# classifier = svm.svc(kernel='linear', C=1.0)
-# classifier.fit(X_train,y_train)
+num_of_train = training_set.shape[0]
+num_of_test = test_set.shape[0]
+
+# testing purpose: dimension check
+print 'Training set has ' + str(num_of_train) + ' rows'
+print 'Test set has ' + str(num_of_test) + ' rows'
+
+X_train = training_set.iloc[:,1:]
+y_train = training_set.iloc[:,0]
+X_test = test_set.iloc[:,1:]
+y_test = test_set.iloc[:,0] 
+
+svm_classifier = SVC(kernel='poly', degree=2)
+svm_classifier.fit(X_train,y_train)
+y_hat = svm_classifier.predict(X_test) # this is a tuple
+
+correct_num = 0.0
+for i in range(0,num_of_test):
+	if y_hat[i]==y_test.iloc[i]:
+		correct_num = correct_num + 1
+
+print('Accuracy: '),
+print(correct_num/num_of_test)
